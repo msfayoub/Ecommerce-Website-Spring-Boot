8bfd567c720e18b6c126d463eca94b0f26737ec5
msfayoub <Ayoub.MOUSSAIF@um6p.ma>
2025-11-22 20:40:30 +0100

    Fix Jenkinsfile syntax error

--- src/test/resources/cleanup.sql
+++
@@ -1,6 +0,0 @@
--- Clean up test data in reverse order of foreign key dependencies
-DELETE FROM order_basket WHERE id IN (1, 2, 999);
-DELETE FROM product WHERE id IN (1, 2, 3, 4, 5, 6, 7, 12, 999);
-DELETE FROM user WHERE id IN (1, 999);
-DELETE FROM category WHERE id IN (1, 2, 3, 4, 5, 6, 999);
-DELETE FROM vendor WHERE id IN (1, 2, 3, 999);
